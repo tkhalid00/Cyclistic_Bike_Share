f09bdb3257d81265167b63b3c68216b8e7031369
Your GitHub Username <your_email@example.com>
2021-06-06 01:10:29 -0400

    .rdata file created and imported into R environment

--- cyclistic_bike_share_rmark.md
+++ cyclistic_bike_share_rmark.md
@@ -53,3 +53,110 @@ library(scales) # This package helps simplify scales while drawing different gra
 library(cowplot) # This package is helpful if multiple plots are required to be produced or saved side by side.
 ```
 
+Next we need to import data into R studio to have an initial look of the given data. Due to large size of `*.csv` files, this code will not be executed in following chunks, and, hence will be commented out.  
+
+First step to import these files into R is to get all the names of `*.csv` data files in the folder where these files are stored. Since I used `data/unconditioned_data` folder to download and store these files, I have given the path and used `list.files` of all `.csv` files in given folder.
+
+
+```r
+# # get all the file names with their path in a variable ===========
+# 
+# list_of_files <- list.files("data/unconditioned_data", pattern = "*.csv", 
+#                             full.names = TRUE)
+# list_of_files
+# class(list_of_files)
+# length(list_of_files)
+```
+
+Before moving further, instead of importing all the files together, its a good practice that we select a single file from above created list and import to check its structure and get a feel of raw data. Again, due to high volume these `.csv` files, this code will not be executed here. We will start our analysis once we get all the data in a single file that is compatible with R and easy to use to maipulate and analyze data.
+
+
+```r
+# # before combining these files, we need to make sure that these files are being
+# # combined in the right format. To ascertain the right file structure, import a 
+# # a single file into R, and check the variables and define the variable type,
+# # if required
+# 
+# check_file_stru <- read_csv(list_of_files[1])
+# str(check_file_stru)
+# glimpse(check_file_stru)
+# colnames(check_file_stru)
+```
+
+
+As these `.csv` file names are stored as a list in R, the next step is to import these raw `.csv` files into R as as a data frame. As this data frame is still not in compatible R format that we need, this code will also not be executed here. In `read_csv()`, we use `col_types = `, the types of columns that we need in our desired data frame are defined at the time of data import. By doing this, we get all the data in a uniform format that is compatible with each other and will remove and errors in future due to data incompatibility. `col_types` abbreviations represent the following:
+* c = `character`
+* f = `factor`
+* T = `dateTime` of class `POSIXct`
+* d = `double`
+
+
+```r
+# # To Combine Data into a Data Frame =======
+# 
+# # use map_df function to match all the extracted data into a data frame variable
+# # in map_df function, '~' is used to define a modified function
+# # read_csv() is originally designed to read a single file, but '~' shows that
+# # the function has been modified to read a whole list instead of reading a 
+# # single file
+# 
+# tripdata_df <- list_of_files %>% 
+#   map_df(~read_csv(.x, col_types = "cfTTccccddddf"))
+# tripdata_df
+# 
+# dim(tripdata_df)
+# 
+# tripdata_df
+```
+
+After import of 12 no. of `.csv` into a single R compatible data frame vector, we need to save it in `.rdata` format. There are several advantages of doing so:
+1. It takes less than one-fourth of the original file size.
+2. `.rdata` files can be imported easily into `R` Environment and they can be loaded instantly without a requirement of doing any data format conversions.
+Now, this master data will be saved as `.rdata` file
+
+
+```r
+# # Save and .Rdata for saving data files ======
+# 
+# # Use "Save" and ".rdata" extension to reduce storage size of data file
+# # efficient write function for csv but takes a fraction (1/4th) of space as compared
+# # to write_csv function
+# 
+
+# save(tripdata_df, file = "data/master_data.rdata")
+```
+
+Finally, we are able to get our hands on a `.rdata` file that can be easily loaded into `R` environment, so from now on all the code chunks will be compiled and executed. First load `.rdata` file into the system.
+
+
+```r
+# Another advantage of .rdata file is that it can be easily loaded into R
+# environment within seconds . This file can be carried anywhere in any other 
+# R project, without the need of carrying original bulk spaced .csv files.
+# load the .rdata file
+
+load("data/master_data.rdata")
+
+glimpse(tripdata_df)
+```
+
+```
+## Rows: 3,489,748
+## Columns: 13
+## $ ride_id            <chr> "A847FADBBC638E45", "5405B80E996FF60D", "5DD24A79A4~
+## $ rideable_type      <fct> docked_bike, docked_bike, docked_bike, docked_bike,~
+## $ started_at         <dttm> 2020-04-26 17:45:14, 2020-04-17 17:08:54, 2020-04-~
+## $ ended_at           <dttm> 2020-04-26 18:12:03, 2020-04-17 17:17:03, 2020-04-~
+## $ start_station_name <chr> "Eckhart Park", "Drake Ave & Fullerton Ave", "McClu~
+## $ start_station_id   <chr> "86", "503", "142", "216", "125", "173", "35", "434~
+## $ end_station_name   <chr> "Lincoln Ave & Diversey Pkwy", "Kosciuszko Park", "~
+## $ end_station_id     <chr> "152", "499", "255", "657", "323", "35", "635", "38~
+## $ start_lat          <dbl> 41.8964, 41.9244, 41.8945, 41.9030, 41.8902, 41.896~
+## $ start_lng          <dbl> -87.6610, -87.7154, -87.6179, -87.6975, -87.6262, -~
+## $ end_lat            <dbl> 41.9322, 41.9306, 41.8679, 41.8992, 41.9695, 41.892~
+## $ end_lng            <dbl> -87.6586, -87.7238, -87.6230, -87.6722, -87.6547, -~
+## $ member_casual      <fct> member, member, member, member, casual, member, mem~
+```
+
+We just have to name the `.rdata` file in destination folder, it will create its `R` variable itself and will be ready to use in seconds. `glimps()' function is on
+
---
+++ script/final-coding-module-Cyclistic_Bikes.R
@@ -0,0 +1,629 @@
+## Load Relevant Library Packages ========
+
+library(tidyverse)
+library(lubridate)
+library(skimr)
+library(scales)
+library(cowplot)
+
+## Import All Relevant Data Files In A Single Data Set =======
+
+# get all the file names with their path in a variable ===========
+
+list_of_files <- list.files("data/unconditioned_data", pattern = "*.csv", 
+                            full.names = TRUE)
+list_of_files
+class(list_of_files)
+length(list_of_files)
+
+
+# before combining these files, we need to make sure that these files are being
+# combined in the right format. To ascertain the right file structure, import a 
+# a single file into R, and check the variables and define the variable type,
+# if required
+
+check_file_stru <- read_csv(list_of_files[1])
+str(check_file_stru)
+glimpse(check_file_stru)
+colnames(check_file_stru)
+
+
+# To Combine Data into a Data Frame =======
+
+# use map_df function to match all the extracted data into a data frame variable
+# in map_df function, '~' is used to define a modified function
+# read_csv() is originally designed to read a single file, but '~' shows that
+# the function has been modified to read a whole list instead of reading a 
+# single file
+
+tripdata_df <- list_of_files %>% 
+  map_df(~read_csv(.x, col_types = "cfTTccccddddf"))
+tripdata_df
+
+dim(tripdata_df)
+
+tripdata_df
+
+
+# Save and .Rdata for saving data files ======
+
+# Use "Save" and ".rdata" extension to reduce storage size of data file
+# efficient write function for csv but takes a fraction (1/4th) of space as compared
+# to write_csv function
+
+save(tripdata_df, file = "data/master_data.rdata")
+
+
+# Another advantage of .rdata file is that it can be easily loaded into R
+# environment within seconds . This file can be carried anywhere in any other 
+# R project, without the need of carrying original bulk spaced .csv files.
+# load the .rdata file
+
+load("data/master_data.rdata")
+
+glimpse(tripdata_df)
+
+
+## Graphing - To Check the Relationship Between Various Variables =====
+
+# qplot(data = tripdata_df, rideable_type, member_casual)
+
+
+
+# hist(tripdata_df$started_at, col = "orange", breaks = 200)
+
+# table(tripdata_df$member_casual, tripdata_df$rideable_type) %>% 
+#  barplot(legend = TRUE)
+
+# Make a copy of original dataset to avoid over writing original dataset
+
+df2 <- tripdata_df
+
+
+# make a new data frame with this information
+save(df2, file = "data/df2.rdata")
+
+
+## Load 'df2' dataset
+
+load("data/df2.rdata")
+
+df2
+class(df2)
+
+# Data wrangling ==============
+
+
+# introduce a new column variable that can calculate trip length
+# use duration calculation operator to calculate the trip duration
+
+
+trip_dur <- df2 %>% 
+  with(started_at %--% ended_at) %>%
+  as.duration() %>% 
+  as.numeric("minutes") %>% 
+  round(2)
+
+trip_dur
+
+
+# add this new useful variable to the data frame under observation
+
+df2 <- df2 %>% 
+  mutate(trip_dur)
+
+glimpse(df2)
+summary(df2$trip_dur)
+
+
+# add a variable of type Date, it will make it easier to analyze data
+
+df2$date <- as.Date(df2$started_at)
+glimpse(df2)
+
+
+
+# to view two plots together
+par(mfrow = c(1, 2), cex = 0.7)
+
+hist(df2$trip_dur)
+boxplot(df2$trip_dur)
+
+
+
+# add a weekday column
+
+df2$day <- factor(format(df2$date, "%a"),
+                    level = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))
+class(df2$day)
+df2$day
+
+# add a month column
+
+df2$month <- factor(format(df2$date, "%b"),
+                    level = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+                              "Aug", "Sep", "Oct", "Nov", "Dec"))
+class(df2$month)
+df2$month
+
+glimpse(df2)
+
+
+# add a time column to create time bins for future analysis
+
+df2$time <- factor(format(df2$started_at, "%H"), 
+                   levels = c("01", "02", "03", "04", "05", "06",
+                              "07", "08", "09", "10", "11", "12",
+                              "13", "14", "15", "16", "17", "18",
+                              "19", "20", "21", "22", "23", "00"))
+
+df2$time
+class(df2$time)
+
+dev.off()
+par(mfrow = c(1, 2), cex = 0.7)
+barplot(table(df2$month))
+barplot(table(df2$day))
+
+
+df2 %>% filter(is.na(month))
+df2 %>% filter(is.na(day))
+
+
+
+neg_dur <- df2 %>% 
+  filter(trip_dur < 0)
+dim(neg_dur)
+
+
+summary(neg_dur$trip_dur)
+
+
+dev.off()
+par(mfrow = c(1, 2), cex = 0.7)
+hist(neg_dur$trip_dur)
+boxplot(neg_dur$trip_dur)
+
+
+neg_dur %>% 
+  select(started_at, ended_at, trip_dur) %>% 
+  filter(trip_dur < -100) %>% 
+  sort(trip_dur, decreasing = TRUE)
+
+df2 %>% filter(trip_dur < 5 & trip_dur > 0)
+
+
+
+# create a new data frame and remove negative values from the data frame
+
+df3 <- df2 %>% 
+  filter(trip_dur >= 0 )
+
+
+save(df3, file = "data/df3.rdata")
+
+load("data/df3.rdata")
+glimpse(df3)
+
+dev.off()
+par(mfrow = c(1, 2), cex = 0.7)
+hist(df3$trip_dur)
+boxplot(df3$trip_dur)
+
+summary(df2$trip_dur)
+summary(df3$trip_dur)
+
+dev.off()
+par(mfrow = c(1, 2), cex = 0.7)
+
+hist(df3$trip_dur, main = "Histogram - Trip Duration", xlab = "Trip Duration - min",
+     ylab = "no of trips")
+boxplot(df3$trip_dur, horizontal = TRUE, main = "Histogram - Trip Duration", 
+        xlab = "Trip Duration - min",
+        ylab = "no of trips")
+
+
+ggplot(data = df3) +
+  geom_bar(aes(x = day), fill = "light blue") +
+  xlab("Weekdays") +
+  ylab("No. of Trips") +
+  ggtitle("Total no. of Trips wrt. Weekdays") +
+  scale_y_continuous(labels = scales::comma)
+
+ggsave("fig_out/01_weekdays_total_trips.png")
+
+
+
+ggplot(df3) +
+  geom_point(aes(x = day, y = trip_dur)) +
+  xlab("Weekdays") +
+  ylab("Trip Duration (in Mins.)") +
+  ggtitle("Trip durations by Weekdays") +
+  scale_y_continuous(labels = scales::comma)
+
+ggsave("fig_out/02_trip_dur_wrt_weekdays_point_plot.png")
+
+
+# Draw total number of trips in member and casual segregation
+
+ggplot(data = df3, aes(x = member_casual, fill = member_casual)) +
+  geom_bar(aes(position = "dodge")) +
+  xlab("Weekdays") +
+  ylab("No. of Trips") +
+  ggtitle("Trip durations by Weekdays") +
+  scale_y_continuous(labels = scales::comma)
+
+
+# Define a theme for your graphs =========
+
+theme_for_graphs <- theme(
+  legend.title = element_blank(), 
+  legend.position = "right", 
+  axis.text.x = element_text(angle = 0, vjust = 0.5, hjust = 0))
+
+theme_for_graphs_v <- theme(
+  legend.title = element_blank(), 
+  legend.position = "right", 
+  axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0))
+
+
+
+# Draw total number of trips in member and casual segregation - Percentage
+
+ggplot(data = df3, aes(x = member_casual, fill = member_casual)) +
+  geom_bar(aes(y = (..count..)/sum(..count..), position = "dodge")) +
+  xlab("Member Status") +
+  ylab("Percentage") +
+  ggtitle("Trip Proportions - Member vs. Casual") +
+  scale_y_continuous(labels = scales::percent)
+
+
+plot_grid(
+  ggplot(data = df3, aes(x = member_casual, fill = member_casual)) +
+    geom_bar(aes(position = "dodge")) +
+    labs(x = "Member Status", y = "no_of_trips", title = "Total Trip Segregation \nMember vs. Casual") +
+    theme_for_graphs +
+    scale_y_continuous(labels = scales::comma),
+  ggplot(data = df3, aes(x = member_casual, fill = member_casual)) +
+    geom_bar(aes(y = (..count..)/sum(..count..), position = "dodge")) +
+    labs(x = "Member Status", y = "Percentage", title = " \n ") +
+    theme_for_graphs +
+    scale_y_continuous(labels = scales::percent)
+  
+)
+
+
+ggsave("fig_out/03_no_of_trips_member_vs_casual.png")
+
+
+ggplot(data = df3, aes(x = day)) +
+  geom_bar(aes(fill = member_casual), position = "dodge") +
+  xlab("Weekdays") +
+  ylab("No. of Trips") +
+  ggtitle("Trip durations by Weekdays") +
+  theme_for_graphs +
+  scale_y_continuous(labels = scales::comma)
+
+
+df4 <- df3 %>% 
+  select(rideable_type, start_station_id, start_station_name, end_station_id,
+         end_station_name,  member_casual, trip_dur, date, day, month, time)
+
+save(df4, file = "data/df4.rdata")
+
+load("data/df4.rdata")
+glimpse(df4)
+
+
+ggplot(data = df4) +
+  geom_bar(aes(x = day, fill = member_casual), position = "dodge") +
+  labs(x = "Weekdays", y = "No. of Trips", 
+       title = "Total no. of Trips wrt. Weekdays") +
+  theme(legend.title = element_blank()) +
+  scale_y_continuous(labels = scales::comma)
+
+ggsave("fig_out/01_1_weekdays_total_trips.png")
+
+
+
+# graph Busy hours round the clock
+ggplot(df4, aes(x = time)) +
+  geom_bar(aes(fill = member_casual), position = "dodge") +
+  theme(legend.title = element_blank()) +
+  labs(x = "time (hours)", y = "no. of trips", 
+       title = "Trips - Round the Clock") +
+  scale_y_continuous(labels = scales::comma)
+
+
+# Create a new data frame to filter Saturday and Sunday traffic
+
+df5_sat_sun <- df4 %>% 
+  filter(day == "Sat" | day == "Sun")
+
+df5_sat_sun
+
+
+plot_grid(
+  ggplot(data = df5_sat_sun, aes(x = day)) +
+    geom_bar(aes(fill = member_casual), position = "dodge") +
+    labs(x = "Weekdays", y = "No. of Trips", 
+         title = "Total no. of Trips wrt. Weekdays") +
+    theme(legend.title = element_blank(), legend.position = "none",
+          axis.text.x = element_text(angle = 0, vjust = 0.5, hjust = 0)) +
+    scale_y_continuous(labels = scales::comma),
+  ggplot(df5_sat_sun, aes(x = time)) +
+    geom_bar(aes(fill = member_casual), position = "dodge") +
+    theme(legend.title = element_blank(), legend.position = "right",
+          axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0)) +
+    labs(x = "time (hours)", y = "no. of trips", 
+         title = "Trips - Round the Clock") +
+    scale_y_continuous(labels = scales::comma)
+)
+
+
+ggsave("fig_out/05_sat_sun_trips.png")
+
+
+ggplot(df5_sat_sun, aes(x = day)) +
+    geom_bar(aes(fill = member_casual), position = "dodge") +
+  labs(title = "Cycle Usage during the Day - Round the Clock", 
+       x = "Time in Hours", y = "No. of Trips") +
+  theme(legend.title = element_blank(), legend.position = "right",
+        axis.text.x = element_blank()) +
+  scale_y_continuous(labels = scales::comma) +
+  facet_grid(day ~ time)
+
+
+ggsave("fig_out/06_sat_sun_trips_pattern.png")
+
+
+
+plot_grid(
+  ggplot(data = df5_sat_sun, aes(x = member_casual, fill = member_casual)) +
+    geom_bar(aes(position = "dodge")) +
+    labs(x = "Member Status", y = "no_of_trips", title = "Total Trips \nSaturdays & Sundays Only") +
+    theme(legend.position = "none") +
+    scale_y_continuous(labels = scales::comma),
+  ggplot(data = df5_sat_sun, aes(x = member_casual, fill = member_casual)) +
+    geom_bar(aes(y = (..count..)/sum(..count..), position = "dodge")) +
+    labs(x = "Member Status", y = "Percentage", title = " \n ") +
+    theme_for_graphs +
+    scale_y_continuous(labels = scales::percent)
+)
+
+
+ggsave("fig_out/07_no_of_trips_sat_sun.png")
+
+
+
+
+plot_grid(
+  ggplot(data = df4, aes(x = member_casual, fill = member_casual)) +
+    geom_bar(aes(position = "dodge")) +
+    labs(x = "Member Status", y = "no_of_trips", title = "Total Trips \nAll Weekdays") +
+    theme(legend.position = "none") +
+    scale_y_continuous(labels = scales::comma),
+  ggplot(data = df5_sat_sun, aes(x = member_casual, fill = member_casual)) +
+    geom_bar(aes(position = "dodge")) +
+    labs(x = "Member Status", y = "", title = "Total Trips \nSaturdays & Sundays Only") +
+    theme(legend.position = "none", legend.title = element_blank()) +
+    scale_y_continuous(labels = scales::comma)
+)
+
+ggsave("fig_out/08_no_of_trips_weekdays_vs_weekend.png")
+
+
+plot_grid(
+  ggplot(data = df4, aes(x = member_casual, fill = member_casual)) +
+    geom_bar(aes(y = (..count..)/sum(..count..), position = "dodge")) +
+    labs(x = "Member Status", y = "Percentage", title = "Total Trips \nAll Weekdays") +
+    theme(legend.position = "none") +
+    scale_y_continuous(labels = scales::percent),
+  ggplot(data = df5_sat_sun, aes(x = member_casual, fill = member_casual)) +
+    geom_bar(aes(y = (..count..)/sum(..count..), position = "dodge")) +
+    labs(x = "Member Status", y = "", title = "Total Trips \nSaturdays & Sundays Only") +
+    theme_for_graphs +
+    scale_y_continuous(labels = scales::percent)
+)  
+
+ggsave("fig_out/09_no_of_trips_weekdays_vs_weekend.png")
+
+
+
+# Calculate average duration of the trip w.r.t. weekdays
+
+avg_trip_duration_day <- df4 %>%  
+  group_by(day, member_casual) %>% 
+  summarise(mean_trip_length = mean(trip_dur))
+
+avg_trip_duration_day
+
+ggplot(avg_trip_duration_day, aes(x = day, y= mean_trip_length, fill = member_casual)) +
+  geom_col(position = "dodge") +
+  labs(title = "Avg. Trip Duration During the Week", x = "Day",
+       y = "Avg. Trip Duration \n(in minutes)") +
+  theme(legend.title = element_blank())
+
+ggsave("fig_out/10_avg_trip_dura.png")
+
+
+
+avg_trip_duration_hour <- df4 %>% 
+  group_by(time, member_casual) %>% 
+  summarise(mean_trip_dura_hour = mean(trip_dur))
+
+avg_trip_duration_hour
+
+ggplot(avg_trip_duration_hour, aes(x = time, y = mean_trip_dura_hour, 
+                                   fill = member_casual)) +
+  geom_col(position = "dodge") +
+  labs(title = "Avg. Trip Duration at each Hour Round the Clock", x = "time",
+       y = "Avg. Trip Duration \n(in minutes)") +
+  theme(legend.title = element_blank())
+
+
+
+df_trip_start_station <- df4 %>% 
+  count(start_station_id, start_station_name, sort = TRUE)
+
+df_trip_start_station
+
+df_trip_end_station <- df4 %>% 
+  count(end_station_id, end_station_name, sort = TRUE)
+
+df_trip_end_station
+
+df_trip_station <- df_trip_start_station %>% 
+  filter(df_trip_start_station$n > quantile(df_trip_start_station$n, probs = 0.75))
+
+df_trip_station
+
+df_trip_station <- df_trip_station %>% 
+  filter(start_station_id %in% df4)
+
+
+
+
+df4 %>% 
+  group_by(start_station_id, start_station_name) %>% 
+  summarise(mean_trip_dur_station = mean(trip_dur)) %>% 
+  arrange(desc(mean_trip_dur_station))
+  
+  
+  
+  
+  
+  
+
+summary(df_trip_station$n)
+quantile(df_trip_station$n, probs = 0.75)
+
+
+df_trip_station %>% 
+
+
+
+
+
+
+avg_trip_duration_member_type <- df3 %>% 
+  select(started_at, month, day, trip_dur, member_casual, rideable_type) %>% 
+  group_by(member_casual) %>% 
+  summarise(mean_trip_length = mean(trip_dur))
+
+avg_trip_duration_member_type
+
+
+
+
+
+weekday_trip_count <- df3 %>% 
+  group_by(day) %>% 
+  count()
+
+weekday_trip_count
+
+
+
+plot_month_trip_count <- ggplot(data = df3) +
+  geom_bar(aes(x = month), fill = "light blue") +
+  scale_y_continuous(labels = scales::comma)
+
+plot_month_trip_count
+
+
+ggplot(df3) + 
+  geom_bar(mapping = aes(x = month, fill = rideable_type)) +
+  scale_y_continuous(labels = scales::comma)
+
+
+
+
+ggplot(df3) + 
+  geom_bar(mapping = aes(x = member_casual, fill = rideable_type)) +
+  scale_y_continuous(labels = scales::comma)
+
+
+ggplot(avg_trip_duration) + 
+  geom_bar(mapping = aes(x = member_casual, fill = trip_dur)) +
+  scale_y_continuous(labels = scales::comma)
+
+ggplot(df3) + 
+  geom_boxplot(mapping = aes(x = date, y = member_casual, color = member_casual))
+
+
+ggplot(df3) + 
+  geom_bar(mapping = aes(x = member_casual, fill = rideable_type)) +
+  scale_y_continuous(labels = scales::comma)
+
+
+
+
+
+
+
+
+
+
+avg_trip_length <- df2 %>% 
+  group_by(start_station_name, Order = FALSE) %>% 
+  summarise(avg_trip = mean(trip_leng), units = "mins")
+  
+avg_trip_length
+
+
+
+
+
+
+
+
+
+
+
+view(avg_trip_length)
+most_common_start <- tripdata_df2 %>% 
+  count(start_station_name, sort = TRUE)
+view(most_common_start)
+
+most_common_end <- tripdata_df2 %>% 
+  count(end_station_name, sort = TRUE) %>% 
+  top_n(5)
+view(most_common_end)
+
+most_common <- tripdata_df2 %>% 
+  count(start_station_name, end_station_name, sort = TRUE)
+  
+view(most_common)
+
+avg_trip_length <- tripdata_df2 %>% 
+  summarise(mean(trip_leng))
+
+class(tripdata_df2$trip_leng)
+
+avg_trip_length
+
+
+str(tripdata_df2$trip_leng)
+
+
+
+
+ggplot(tripdata_df) + 
+  geom_histogram(mapping = aes(x = started_at))
+
+ggplot(tripdata_df) + 
+  geom_boxplot(mapping = aes(x = member_casual, y = started_at))
+
+ggplot(tripdata_df) + 
+  geom_boxplot(mapping = aes(x = member_casual, y = started_at), fill = "red")
+
+ggplot(tripdata_df) + 
+  geom_bar(mapping = aes(x = member_casual, fill = rideable_type))
+
+tripdata_df %>% 
+  filter(rideable_type == "docked_bike") %>% 
+  ggplot(aes(x = member_casual, fill = rideable_type)) + geom_bar()
+
+ggplot(tripdata_df) + 
+  geom_bar(mapping = aes(x = member_casual, position = "dodge"))
